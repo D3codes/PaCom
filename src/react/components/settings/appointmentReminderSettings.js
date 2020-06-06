ff9ad102118dc27b9cffa706abf2c1c528c88eb7
David Freeman <freeman@civicplus.com>
2020-06-06 08:30:21 -0500

    began work

--- src/react/components/settings/appointmentReminderSettings.js
+++ src/react/components/settings/appointmentReminderSettings.js
@@ -1,8 +1,53 @@
-import React from 'react';
-import { Typography } from '@material-ui/core';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Typography, Divider } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
-export default function AppointmentRemindersSettings() {
+const useStyles = makeStyles(theme => ({
+	root: {
+		display: 'flex',
+		flexDirection: 'column',
+		height: '100%'
+	}
+}));
+
+export default function AppointmentRemindersSettings({ appointmentReminders, reloadSettings, hasWritePermission }) {
+	const classes = useStyles();
+	const [numberOfDays, setNumberOfDays] = useState(appointmentReminders.dateVerification.numberOfDays);
+	const [endOfRange, setEndOfRange] = useState(appointmentReminders.dateVerification.endOfRange);
+	const [allowSendOutsideRange, setAllowSendOutsideRange] = useState(appointmentReminders.dateVerification.allowSendOutsideRange);
+	const [useBusinessDays, setUseBusinessDays] = useState(appointmentReminders.dateVerification.useBusinessDays);
+	const [sendToPreferredAndSms, setSendToPreferredAndSms] = useState(appointmentReminders.notificationMethod.sendToPreferredAndSms);
+	const [textHomeIfCellNotAvailable, setTextHomeIfCellNotAvailable] = useState(appointmentReminders.notificationMethod.textHomeIfCellNotAvailable);
+
 	return (
-		<Typography variant="h2">This is the Appointment Reminder Settings Content</Typography>
+		<div className={classes.root}>
+			<div className={classes.notificationMethodContainer}>
+				<Typography>Notification Method</Typography>
+			</div>
+			<Divider />
+			<div className={classes.dateVerificationContainer}>
+				<Typography>Date Verification</Typography>
+			</div>
+		</div>
 	);
 }
+
+AppointmentRemindersSettings.propTypes = {
+	appointmentReminders: PropTypes.shape(
+		{
+			dateVerification: {
+				numberOfDays: PropTypes.number,
+				endOfRange: PropTypes.number,
+				allowSendOutsideRange: PropTypes.bool,
+				useBusinessDays: PropTypes.bool
+			},
+			notificationMethod: {
+				sendToPreferredAndSms: PropTypes.bool,
+				textHomeIfCellNotAvailable: PropTypes.bool
+			}
+		}
+	).isRequired,
+	reloadSettings: PropTypes.func.isRequired,
+	hasWritePermission: PropTypes.bool.isRequired
+};
