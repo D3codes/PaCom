2c9176c0175d92eed026ecc6e63f5c245a3b57a1
David Freeman <davidfreeman@davids-mbp.lan>
2020-11-20 22:50:59 -0600

    continued working on date verification

--- src/react/components/settings/appointmentReminderSettings.js
+++ src/react/components/settings/appointmentReminderSettings.js
@@ -29,34 +29,48 @@ export default function AppointmentRemindersSettings({ appointmentReminders, rel
 			<Divider />
 			<div className={classes.dateVerificationContainer}>
 				<Typography variant="h5">Date Verification</Typography>
-				<Typography>Reminders should be sent</Typography>
+				<Typography variant="h5">Reminders should be sent</Typography>
 				<FormControl>
 					<Select
 					value={!endOfRange}
-					onChange={event => {setEndOfRange(5);}}
+					onChange={event => {setEndOfRange(endOfRange ? undefined : 5);}}
 					inputProps={{ 'aria-label': 'Without label' }}
 					>
 					<MenuItem value={true}>Exactly</MenuItem>
 					<MenuItem value={false}>Between</MenuItem>
 					</Select>
 				</FormControl>
-				<TextField type="number" value={numberOfDays} onChange={event => {}}/>
+				<Typography display="inline"> </Typography>
+				<TextField
+					type="number"
+					value={numberOfDays}
+					style={{width: 35}}
+					onChange={event => {setNumberOfDays(parseInt(event.target.value))}}
+					InputProps={{ inputProps: { min: 0 } }}
+				/>
 				{ endOfRange && <>
-					<Typography display="inline"> - </Typography>
-					<TextField type="number" value={endOfRange} onChange={event => {}}/>
+					<Typography variant="h5" display="inline" style={{bottom: 0}}> to </Typography>
+					<TextField
+						type="number"
+						value={endOfRange}
+						style={{width: 35}}
+						onChange={event => {setEndOfRange(parseInt(event.target.value))}}
+						InputProps={{ inputProps: { min: numberOfDays+1 } }}
+					/>
 				</>
 				}
+				<Typography display="inline"> </Typography>
 				<FormControl>
 					<Select
 					value={!useBusinessDays}
 					onChange={event => {}}
 					inputProps={{ 'aria-label': 'Without label' }}
 					>
-					<MenuItem value={true}>days</MenuItem>
-					<MenuItem value={false}>business days</MenuItem>
+					<MenuItem value={true}>day(s)</MenuItem>
+					<MenuItem value={false}>business day(s)</MenuItem>
 					</Select>
 				</FormControl>
-				<Typography>before appointment.</Typography>
+				<Typography variant="h5">before appointment.</Typography>
 			</div>
 		</div>
 	);
