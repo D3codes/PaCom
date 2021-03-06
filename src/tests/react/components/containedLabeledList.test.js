4257d05f93cec8ecb33aa3008c73a97f2a251cc7
David Freeman <davidfreeman@davids-mbp.lan>
2021-03-06 10:05:07 -0600

    added containedlabeledlist test

--- src/tests/react/components/containedLabeledList.test.js
+++ src/tests/react/components/containedLabeledList.test.js
@@ -3,15 +3,29 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import ContainedLabeledList from '../../../react/components/containedLabeledList';
 
+const testItems = [
+	{ name: '1', value: 'one' },
+	{ name: '2', value: 'two' }
+];
+
 describe('ContainedLabeledList', () => {
 	it('renders without crashing', () => {
 		const { container } = render(<ContainedLabeledList onClick={jest.fn()} />);
+
 		expect(container.firstChild.className.includes('containedLabeledListContainer')).toBe(true);
+	});
+
+	it('renders the list label and items without crashing', () => {
+		const { getByText } = render(<ContainedLabeledList onClick={jest.fn()} label="Test Label" items={testItems} />);
+
+		expect(getByText('Test Label'));
+		expect(getByText('1'));
+		expect(getByText('2'));
 	});
 
 	it('calls the onClick function when an item is clicked', () => {
 		const onClickMock = jest.fn();
-		const { getByText } = render(<ContainedLabeledList onClick={onClickMock} items={[{ name: '1', value: 'one' }]} />);
+		const { getByText } = render(<ContainedLabeledList onClick={onClickMock} items={testItems} />);
 
 		expect(getByText('1'));
 		fireEvent.click(getByText('1'));
