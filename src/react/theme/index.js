0102e66cdf5823ce690db007a2f3398bddf449c6
caullens <greasyw00t@gmail.com>
2021-03-15 15:20:49 -0500

    Break up theme pieces into separate files

--- src/react/theme/index.js
+++ src/react/theme/index.js
@@ -1,118 +1,97 @@
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 
+import palette from './palette';
+import shape from './shape';
+import typography from './typography';
+import mixins from './mixins';
+
 // eslint-disable-next-line import/no-mutable-exports
-let theme = createMuiTheme({
-	palette: {
-		primary: {
-			light: '#63ccff',
-			main: '#009be5',
-			dark: '#006db3'
-		}
-	},
-	typography: {
-		h5: {
-			fontWeight: 500,
-			fontSize: 26,
-			letterSpacing: 0.5
-		}
-	},
-	shape: {
-		borderRadius: 8
-	},
-	props: {
-		MuiTab: {
-			disableRipple: true
-		}
-	},
-	mixins: {
-		toolbar: {
-			minHeight: 48
-		}
-	}
+const theme = createMuiTheme({
+	palette,
+	typography,
+	shape,
+	mixins
 });
 
-theme = {
-	...theme,
-	overrides: {
-		MuiDrawer: {
-			paper: {
-				backgroundColor: '#18202c'
-			}
+const overrides = {
+	MuiDrawer: {
+		paper: {
+			backgroundColor: '#18202c'
+		}
+	},
+	MuiButton: {
+		label: {
+			textTransform: 'none'
 		},
-		MuiButton: {
-			label: {
-				textTransform: 'none'
-			},
-			contained: {
-				boxShadow: 'none',
-				'&:active': {
-					boxShadow: 'none'
-				}
+		contained: {
+			boxShadow: 'none',
+			'&:active': {
+				boxShadow: 'none'
 			}
+		}
+	},
+	MuiTabs: {
+		root: {
+			marginLeft: theme.spacing(0)
 		},
-		MuiTabs: {
-			root: {
-				marginLeft: theme.spacing(1)
-			},
-			indicator: {
-				height: 3,
-				borderTopLeftRadius: 3,
-				borderTopRightRadius: 3,
-				backgroundColor: theme.palette.common.white
-			}
-		},
-		MuiTab: {
-			root: {
-				textTransform: 'none',
-				margin: '0 16px',
-				minWidth: 0,
+		indicator: {
+			height: 3,
+			borderTopLeftRadius: 3,
+			borderTopRightRadius: 3,
+			backgroundColor: theme.palette.common.white
+		}
+	},
+	MuiTab: {
+		root: {
+			textTransform: 'none',
+			margin: '0 16px',
+			minWidth: 0,
+			padding: 0,
+			[theme.breakpoints.up('md')]: {
 				padding: 0,
-				[theme.breakpoints.up('md')]: {
-					padding: 0,
-					minWidth: 0
-				}
+				minWidth: 0
 			}
+		}
+	},
+	MuiIconButton: {
+		root: {
+			padding: theme.spacing()
+		}
+	},
+	MuiTooltip: {
+		tooltip: {
+			borderRadius: 4
+		}
+	},
+	MuiDivider: {
+		root: {
+			backgroundColor: '#404854'
+		}
+	},
+	MuiListItem: {
+		gutters: {
+			paddingRight: theme.spacing(1.5),
+			paddingLeft: theme.spacing(1.5)
+		}
+	},
+	MuiListItemText: {
+		primary: {
+			fontWeight: theme.typography.fontWeightMedium,
+			fontSize: 14
 		},
-		MuiIconButton: {
-			root: {
-				padding: theme.spacing()
-			}
-		},
-		MuiTooltip: {
-			tooltip: {
-				borderRadius: 4
-			}
-		},
-		MuiDivider: {
-			root: {
-				backgroundColor: '#404854'
-			}
-		},
-		MuiListItem: {
-			gutters: {
-				paddingRight: theme.spacing(1.5),
-				paddingLeft: theme.spacing(1.5)
-			}
-		},
-		MuiListItemText: {
-			primary: {
-				fontWeight: theme.typography.fontWeightMedium,
-				fontSize: 14
-			},
-			inset: {
-				paddingLeft: 28
-			}
-		},
-		MuiListItemIcon: {
-			root: {
-				marginRight: theme.spacing(),
-				minWidth: 0,
-				'& svg': {
-					fontSize: 20
-				}
+		inset: {
+			paddingLeft: 28
+		}
+	},
+	MuiListItemIcon: {
+		root: {
+			marginRight: theme.spacing(),
+			minWidth: 0,
+			'& svg': {
+				fontSize: 20
 			}
 		}
 	}
 };
 
-export default theme;
+export default { ...theme, overrides };
--- src/react/theme/palette.js
+++ src/react/theme/palette.js
@@ -1,25 +1,8 @@
 const palette = {
 	primary: {
-		main: '#1a547a',
-		light: '#5080a9',
-		dark: '#002c4e',
-		contrastText: '#fafafa'
-	},
-	secondary: {
-		main: '#f4b781',
-		light: '#ffe9b1',
-		dark: '#bf8754',
-		contrastText: '#222222'
-	},
-	background: {
-		paper: '#fafafa',
-		default: '#f0f0f0'
-	},
-	error: {
-		main: '#f46d60',
-		light: '#ff9e8e',
-		dark: '#bc3c36',
-		contrastText: '#222222'
+		light: '#63ccff',
+		main: '#009be5',
+		dark: '#006db3'
 	}
 };
 
