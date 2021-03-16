a141dec3ad3861df608635a746ef2b509119d2fe
David Freeman <davidfreeman@Davids-MacBook-Pro.local>
2021-03-16 15:44:59 -0500

    added to message controller

--- src/react/utilities/messageController.js
+++ src/react/utilities/messageController.js
@@ -1,6 +1,10 @@
 const showInfo = (title, message) => window.ipcRenderer.invoke('show-alert', title, message, 'info', ['OK'], 0, 0);
 
-const showWarning = (title, message) => window.ipcRenderer.invoke('show-alert', title, message, 'warning', ['Continue', 'Cancel'], 0, 1);
+const showError = (title, message) => window.ipcRenderer.invoke('show-alert', title, message, 'error', ['OK'], 0, 0);
+
+const showWarning = (title, message) => window.ipcRenderer.invoke('show-alert', title, message, 'warning', ['OK'], 0, 0);
+
+const showWarningWithContinue = (title, message) => window.ipcRenderer.invoke('show-alert', title, message, 'warning', ['Continue', 'Cancel'], 0, 1);
 
 const showQuestion = (title, message) => window.ipcRenderer.invoke('show-alert', title, message, 'question', ['OK', 'Cancel'], 0, 1);
 
@@ -10,7 +14,9 @@ const confirmSave = (title, message) => window.ipcRenderer.invoke('show-alert', 
 
 export default {
 	showInfo,
+	showError,
 	showWarning,
+	showWarningWithContinue,
 	showQuestion,
 	confirmDelete,
 	confirmSave
