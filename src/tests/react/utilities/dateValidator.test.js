162f422e40dc588e312bfd3cbe274165a6a809e9
David Freeman <freeman@civicplus.com>
2021-03-21 18:20:59 -0500

    fixed linting error;

--- src/tests/react/utilities/dateValidator.test.js
+++ src/tests/react/utilities/dateValidator.test.js
@@ -13,7 +13,7 @@ const startOfTomorrow = () => {
 	const year = now.getFullYear();
 	const month = now.getMonth();
 	const day = now.getDate();
-  
+
 	const date = new Date(0);
 	date.setFullYear(year, month, day + 1);
 	date.setHours(0, 0, 0, 0);
