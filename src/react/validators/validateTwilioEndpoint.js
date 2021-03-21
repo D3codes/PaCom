ee806861ac8bcffa68318ae29f5bea675692eed4
David Freeman <freeman@civicplus.com>
2021-03-21 18:58:09 -0500

    updated twilio endpoint validator to only accept v2 endpoints

--- src/react/validators/validateTwilioEndpoint.js
+++ src/react/validators/validateTwilioEndpoint.js
@@ -1,5 +1,5 @@
 const validateTwilioEndpoint = endpoint => {
-	const regex = /https:\/\/studio.twilio.com\/v[1-2]\/Flows\/[a-zA-Z0-9]+\/Executions$/;
+	const regex = /https:\/\/studio.twilio.com\/v2\/Flows\/[a-zA-Z0-9]+\/Executions$/;
 	const found = endpoint.match(regex);
 	return found ? found[0] : null;
 };
